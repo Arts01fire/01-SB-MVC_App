ec72c195d113326be060654984c129f3d9ae80ff
Yogesh Shandilya <yogeshshandilya01@gmail.com>
2021-02-16 19:24:21 +0530

    Ap modified

--- src/main/java/com/yogesh/Application.java
+++ src/main/java/com/yogesh/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+
+          System.out.println("1st Spring Web Mvc App....!");
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
